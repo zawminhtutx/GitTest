538e889600a056426434286e1585a109a206cc90
Zaw Min Htut <94360823+zawminhtutx@users.noreply.github.com>
2023-02-05 16:23:22 +0630

    Update hellword.java

--- src/Test/hellword.java
+++ src/Test/hellword.java
@@ -5,6 +5,7 @@ public class hellword {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello word");
+		System.out.println("add hello world");
 	}
 
 }
